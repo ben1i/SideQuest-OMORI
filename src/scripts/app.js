1df0f5b663b51765783248e10cf4adcb52249a28
Benjamin <benjamin.brihaye@student.heaj.be>
2026-04-27 14:33:37 +0200

    JSON
    Data for "deepwell-ladder", "deepwell-entrance", "northlake", "vastforest-northcoast", "stumpentrance"

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -146,7 +146,7 @@ fetch('./assets/data/data.json')
             console.log(currentMapElements)
         }
 
-        loadMap('ghostparty');
+        loadMap('stumpentrance');
 
     })
     .catch(function(error) {
