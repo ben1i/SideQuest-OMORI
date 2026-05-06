69b238f4bbef31c5de706b3e03e473f38d3d0d58
Benjamin <benjamin.brihaye@student.heaj.be>
2026-05-06 11:34:24 +0200

    Assets, JSON
    Assets : Added Sunhat Ghost, Glasses Ghost, Propeller Ghost, Berly, Van and Lost Ball
    
    JSON: map data for "otherworld-entrance", "otherworld-ladder", "otherworld-sprout", "otherworld-field", "otherworld", "otherworld-to-junkyard", "otherworld-lake", "otherworld-to-frozenlake", "frozenlake", "junkyard-entrance", "junkyard", "junkyard-right", "junkyard-puzzle", "vastforest-right", "pinwhellforest" + added interactions for Berly's Lost Ball

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -146,7 +146,7 @@ fetch('./assets/data/data.json')
             console.log(currentMapElements)
         }
 
-        loadMap('trainstation-wait');
+        loadMap('vastforest-left');
 
     })
     .catch(function(error) {
